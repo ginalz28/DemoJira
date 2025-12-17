7b82c6e2a59db958a55d9657c17b8ef02a833f14
Hoàng Khánh Huy <hoangkhanhhuy28@gmail.com>
2025-12-17 15:54:07 +0700

    DEMO-1 Test chức năng Đăng nhập

--- LoginService.java
+++ LoginService.java
@@ -2,7 +2,7 @@ public class LoginService {
     public boolean checkLogin(String username, String password) {
         // GIẢ LẬP LỖI (BUG):
         // Yêu cầu đề bài là pass "123456", nhưng Dev code nhầm thành "123"
-        //UPDA
+        //UP
         if ("admin".equals(username) && "123456".equals(password)) {
             return true;
         }
