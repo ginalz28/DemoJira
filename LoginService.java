5b967314c389fd459a7afdd4fb54282883167c83
Hoàng Khánh Huy <hoangkhanhhuy28@example.com>
2025-12-17 16:28:52 +0700

    DEMO-2 Fix dax fix usre

--- LoginService.java
+++ LoginService.java
@@ -2,7 +2,7 @@ public class LoginService {
     public boolean checkLogin(String username, String password) {
         // GIẢ LẬP LỖI (BUG):
         // Yêu cầu đề bài là pass "123456", nhưng Dev code nhầm thành "123"
-        if ("ad".equals(username) && "123456".equals(password)) {
+        if ("admin".equals(username) && "123456".equals(password)) {
             return true;
         }
         return false;
