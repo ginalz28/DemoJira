2d0bc9eb7e03b9a635e32271fcab04defa56d77b
Hoàng Khánh Huy <hoangkhanhhuy28@example.com>
2025-12-17 16:46:42 +0700

    DEMO-1 Fix fix loi

--- LoginService.java
+++ LoginService.java
@@ -1,8 +1,9 @@
 public class LoginService {
     public boolean checkLogin(String username, String password) {
         // GIẢ LẬP LỖI (BUG):
-        // Yêu cầu đề bài là pass "123456", nhưng Dev code nhầm thành "123"
-        if ("admin".equals(username) && "123456".equals(password)) {
+        // Yêu cầu đề bài là pass "123456", nhưng  code nhầm thành "123"
+        //UPDATE
+        if ("admin".equals(username) && "123".equals(password)) {
             return true;
         }
         return false;
